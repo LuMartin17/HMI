d934f2448289ec772c56a7ad7c94d530c94491ff
Lu Martin <146157248+LuMartin17@users.noreply.github.com>
2023-09-28 00:32:27 -0600

    Add files via upload

---
+++ standby.js
@@ -0,0 +1,78 @@
+let dataTable;
+let dataTableIsInitialized = false;
+
+
+const dataTableOptions = {
+    //scrollX: "2000px",
+    lengthMenu: [5, 10, 15, 20, 100, 200, 500],
+    columnDefs: [
+        { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6] },
+        { orderable: false, targets: [5, 6] },
+        { searchable: false, targets: [1] }
+        //{ width: "50%", targets: [0] }
+    ],
+    pageLength: 3,
+    destroy: true,
+    language: {
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        zeroRecords: "Ningún usuario encontrado",
+        info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Ningún usuario encontrado",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        search: "Buscar:",
+        loadingRecords: "Cargando...",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
+    }
+};
+
+const initDataTable = async () => {
+    if (dataTableIsInitialized) {
+        dataTable.destroy();
+    }
+
+    await listUsers();
+
+    dataTable = $("#standby").DataTable(dataTableOptions);
+
+    dataTableIsInitialized = true;
+};
+
+const listUsers = async () => {
+    try {
+        const response = await fetch("standby.json"); // Cambiado a "standby.json"
+        const data = await response.json(); // Cambiado a "data"
+
+        let content = ``;
+        data[2].data.forEach((row, index) => { // Acceso a los datos corregido
+            content += `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${row.nom_equipo}</td>
+                    <td>${row.marca}</td>
+                    <td>${row.modelo}</td>
+                    <td>${row.no_serie}</td>
+                    <td>${row.no_inventario}</td>
+                    <td>${row.ubi}</td>
+                   
+                    <td>
+                        <button class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
+                        <span style="margin-right: 10px;"></span>
+                        <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                    </td>
+                </tr>`;
+        });
+
+        document.getElementById("id_standby").innerHTML = content; // Cambiado a "id_standby"
+    } catch (ex) {
+        alert(ex);
+    }
+};
+
+window.addEventListener("load", async () => {
+    await initDataTable();
+});
